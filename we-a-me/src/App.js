aff45f8872d84f2af1422bbc1d40235ce5eccd25
Danielly Alves Silva <101145664+daniellysilva@users.noreply.github.com>
2022-06-08 10:14:35 -0300

    Update App.js

--- we-a-me/src/App.js
+++ we-a-me/src/App.js
@@ -1,9 +1,10 @@
-
+import NavBar from "./components/navbar/NavBar";
+import NavBarIndex from "./components/navbar/NavBarIndex/NavBarIndex";
 
 function App() {
   return (
     <div>
-      <p>Danielly</p>
+      <NavBarIndex/>
     </div>
   );
 }
