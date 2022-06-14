a42a37d738af1b9f79e9ff4c3e88d8e6a118ac2a
Danielly Alves Silva <101145664+daniellysilva@users.noreply.github.com>
2022-06-14 15:18:46 -0300

    Update NavbarCategorias.jsx

--- we-a-me/src/components/Pages Componentes/Navbar Categorias/NavbarCategorias.jsx
+++ we-a-me/src/components/Pages Componentes/Navbar Categorias/NavbarCategorias.jsx
@@ -4,7 +4,7 @@ import './NavbarCategorias.css';
 export function NavbarCategorias() {
     return (
         <div className="container-fluid" id="servicos">
-            <ul className="nome-categorias nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 mt-0">
+            <ul className="nome-categorias nav col-12 col-md-auto mb-2 justify-content-center  mb-md-0 mt-0">
                 <a href="#" className="nav-link px-4" id="navbarletra">Reformas e Reparo</a>
                 <a href="#" className="nav-link px-2" id="navbarletra">Beleza</a>
                 <a href="#" className="nav-link px-2 " id="navbarletra">Auto</a>
