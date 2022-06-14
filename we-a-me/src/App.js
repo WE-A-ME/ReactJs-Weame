02b06a4f1a398a38f2b5dafd1f462903bbc1fa2b
Tijigui <thiago.santos.guimaraes@outlook.com>
2022-06-14 13:59:51 -0300

    Alteração 02

--- we-a-me/src/App.js
+++ we-a-me/src/App.js
@@ -2,18 +2,18 @@ import  "./App.css";
 import Categorias from "./Paginas/Categorias/Categorias";
 import SobreNos from "./Paginas/SobreNos/SobreNos";
 import { Footer } from "./components/Pages Componentes/Footer/footer";
-import Index from "./Paginas/Index";
-import NavBar from "./components/Pages Componentes/navbar-input/navbar";
-import { NavbarCategorias } from "./components/Pages Componentes/NavbarCategorias/NavbarCategorias";
-import { NavBarIndex } from "./components/NavBarIndex/NavBarIndex";
+import Index from "./Paginas/1-Index";
+import NavBar from "./components/Pages Componentes/NavBar Input/navbar";
+import { NavbarCategorias } from "./components/Pages Componentes/Navbar Categorias/NavbarCategorias";
+import { NavBarIndex } from "./components/Pages Componentes/NavBar Index/NavBarIndex";
 
 
 function App() {
   return (
     <div>
-      <NavBarIndex/>
-      <NavbarCategorias/>
-      {/* <Index/> */}
+      {/* <NavBarIndex/> */}
+      {/* <NavbarCategorias/> */}
+      <Index/>
       {/* <Categorias/> */}
       {/* <SobreNos/> */}
       {/* <Footer/> */}
