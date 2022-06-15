820036d09afc9829f18153c5922ce026f9aed2f2
Danielly Alves Silva <101145664+daniellysilva@users.noreply.github.com>
2022-06-15 07:41:31 -0300

    jsx

--- we-a-me/src/App.js
+++ we-a-me/src/App.js
@@ -1,17 +1,21 @@
 import  "./App.css";
 import { NavBarIndex } from "./components/Pages Componentes/NavBar Index/NavBarIndex";
+import { Footer } from "./components/Pages Componentes/Footer/footer";
 import { NavbarCategorias } from "./components/Pages Componentes/Navbar Categorias/NavbarCategorias";
 import {  Categorias} from "./Paginas/Categorias/Categorias";
+import SobreNos from "./Paginas/SobreNos/SobreNos";
 import {Index} from "./components/Pages Thiago/1-Index/index";
 import { CadastroEmpresa } from "./LogineCadastro/CadastroCliente/cadastro";
+
 
 function App() {
   return (
     <div>
         <NavBarIndex/> 
-        <NavbarCategorias/>
         <Index/> 
         <Categorias/> 
+        <SobreNos/>
+        <Footer/>
     </div>
   );
 }
