77a0f46a016c8ad3b8a56a65bf0992f1080ff2ed
Tijigui <thiago.santos.guimaraes@outlook.com>
2022-06-15 13:02:59 -0300

    Alterações Corretas 01

--- we-a-me/src/App.js
+++ we-a-me/src/App.js
@@ -1,24 +1,26 @@
-import  "./App.css";
+import "./App.css";
 import { NavBarIndex } from "./components/Pages Componentes/NavBar Index/NavBarIndex";
 import { Footer } from "./components/Pages Componentes/Footer/footer";
 import { NavbarCategorias } from "./components/Pages Componentes/Navbar Categorias/NavbarCategorias";
-import {  Categorias} from "./components/Page Danielly/Categorias/Categorias";
+import { Categorias } from "./components/Page Danielly/Categorias/Categorias";
 import { Suporte } from "./components/Pages Thiago/Suporte/Suporte";
 import SobreNos from "./components/Page Danielly/SobreNos/SobreNos";
-import {Index} from "./components/Pages Thiago/1-Index/index";
+import { Index } from "./components/Pages Thiago/1-Index/index";
 import { CadastroEmpresa } from "./components/Pages Thiago/CadastroCliente/cadastro";
+import { Reformas } from "./pages/LoginCategorias/Reformas/LoginCategoriasReformas";
 
 
 function App() {
   return (
     <div>
-        {/* <NavBarIndex/> OK  */}
-        {/* <Index/> Ok */}
-        {/* <CadastroEmpresa/> OK */}
-        {/* <Categorias/> OK */}
-        {/* <Suporte/> OK */}
-        {/* <SobreNos/> OK */}
-        {/* <Footer/> OK */}
+      {/* <NavBarIndex/> OK  */}
+      {/* <Index/> Ok */}
+      {/* <CadastroEmpresa/> OK */}
+      {/* <Categorias/> OK */}
+      {/* <Suporte/> OK */}
+      {/* <SobreNos/> OK */}
+      {/* <Reformas /> Arrumar o Footer */}
+      {/* <Footer/> OK */}
     </div>
   );
 }
--- we-a-me/src/pages/LoginCategorias/Reformas/LoginCategoriasReformas.js
+++ we-a-me/src/pages/LoginCategorias/Reformas/LoginCategoriasReformas.js
@@ -2,147 +2,151 @@ import './LoginCategoriasReformas.css';
 import Profissional from './img/imagemprofissional.jpg';
 import Profissional2 from './img/imagemprofissional2.jpg';
 import Profissional3 from './img/imagemprofissional3.jpg';
+import { NavBarIndex } from '../../../components/Pages Componentes/NavBar Index/NavBarIndex';
+import { NavbarCategorias } from '../../../components/Pages Componentes/Navbar Categorias/NavbarCategorias';
+import { Footer } from '../../../components/Pages Componentes/Footer/footer';
 
-function Reformas() {
-    return (
+export function Reformas() {
+  return (
 
-    <main>
+    <div>
+
+      <NavBarIndex />
+      <NavbarCategorias />
+
+      <main>
         <div className="titulo">
-            <h2>Reformas e Reparo</h2>
+          <h2>Reformas e Reparo</h2>
         </div>
-
         <div class="card mb-3" id="cartao3">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src={Profissional} class="img-fluid " alt="Imagem Profissional do Autônomo" title="Jorge dos Santos" id="imagem3"/>
-            <div id="avaliacao1">
-              <ul class="nav col-12" id="estrelas1">
-                <li id="nota">4.5</li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star-half-stroke"></i></li>
-              </ul>
-              <p class="card-text" id="profissao9"><small class="text-muted">Avaliação Profissional </small></p>
-           </div>
-          </div>
-          <div class="col-md-8">
-            <div class="card-body" id="texto1">
-              <h5 class="card-title" id="nome2">Jorge dos Santos</h5>
-              <p class="card-text" id="profissao9"><small class="text-muted">Especialista em Pinturas e Consertos Residenciais. </small></p>
-              <div class="icones">
-                <ul class="nav col-12" id="informacoes3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src={Profissional} class="img-fluid " alt="Imagem Profissional do Autônomo" title="Jorge dos Santos" id="imagem3" />
+              <div id="avaliacao1">
+                <ul class="nav col-12" id="estrelas1">
+                  <li id="nota">4.5</li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star-half-stroke"></i></li>
+                </ul>
+                <p class="card-text" id="profissao9"><small class="text-muted">Avaliação Profissional </small></p>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body" id="texto1">
+                <h5 class="card-title" id="nome2">Jorge dos Santos</h5>
+                <p class="card-text" id="profissao9"><small class="text-muted">Especialista em Pinturas e Consertos Residenciais. </small></p>
+                <div class="icones">
+                  <ul class="nav col-12" id="informacoes3">
                     <li> <i class="fa-solid fa-location-dot"></i>  Osasco</li>
                     <li> <i class="fa-solid fa-hammer"></i>  17 Serviços</li>
                     <li> <i class="fa-solid fa-circle-check"></i>  Documentos Verificados</li>
-                </ul>
+                  </ul>
+                </div>
+                <div id="texto1">
+                  <p class="card-text">Sou prestativo, educado, competente e procuro atender ao máximo as expectativas dos meus clientes e das empresas. Trocas de luminárias, armários, instalações de antenas, pinturas em domicílios e comércios. </p>
+                </div>
               </div>
-                  <div id="texto1">
-                    <p class="card-text">Sou prestativo, educado, competente e procuro atender ao máximo as expectativas dos meus clientes e das empresas. Trocas de luminárias, armários, instalações de antenas, pinturas em domicílios e comércios. </p>
-                  </div>
+              <button id="botao">
+                <a href="#">Visitar Perfil</a>
+              </button>
             </div>
-            <button id="botao">
-              <a href="#">Visitar Perfil</a>
-            </button>
           </div>
         </div>
-      </div>
-
-      <div class="card mb-3" id="cartao3">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src={Profissional2} class="img-fluid " alt="Imagem Profissional do Autônomo" title="Bárbara Martins" id="imagem3"/>
-            <div id="avaliacao1">
-              <ul class="nav col-12" id="estrelas1">
-                <li id="nota">4.0</li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-              </ul>
-              <p class="card-text" id="profissao9"><small class="text-muted">Avaliação Profissional </small></p>
-           </div>
-          </div>
-          <div class="col-md-8">
-            <div class="card-body" id="texto1">
-              <h5 class="card-title" id="nome2">Bárbara Martins</h5>
-              <p class="card-text" id="profissao9"><small class="text-muted">Auxiliar de Manutenção Mecânica. </small></p>
-              <div class="icones">
-                <ul class="nav col-12" id="informacoes3">
+        <div class="card mb-3" id="cartao3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src={Profissional2} class="img-fluid " alt="Imagem Profissional do Autônomo" title="Bárbara Martins" id="imagem3" />
+              <div id="avaliacao1">
+                <ul class="nav col-12" id="estrelas1">
+                  <li id="nota">4.0</li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                </ul>
+                <p class="card-text" id="profissao9"><small class="text-muted">Avaliação Profissional </small></p>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body" id="texto1">
+                <h5 class="card-title" id="nome2">Bárbara Martins</h5>
+                <p class="card-text" id="profissao9"><small class="text-muted">Auxiliar de Manutenção Mecânica. </small></p>
+                <div class="icones">
+                  <ul class="nav col-12" id="informacoes3">
                     <li> <i class="fa-solid fa-location-dot"></i>  Osasco</li>
                     <li> <i class="fa-solid fa-hammer"></i>  7 Serviços</li>
                     <li> <i class="fa-solid fa-circle-check"></i>  Documentos Verificados</li>
-                </ul>
+                  </ul>
+                </div>
+                <div id="texto1">
+                  <p class="card-text">Olá eu sou a Bárbara, acabei de concluir o curso de Auxiliar Mecânico de Manutenção no SENAI. Eu tenho boas referências dos meu professores e muita motivação em ajudar o meu cliente. Tenho preferência em atender na cidade de Osasco.</p>
+                </div>
               </div>
-                  <div id="texto1">
-                    <p class="card-text">Olá eu sou a Bárbara, acabei de concluir o curso de Auxiliar Mecânico de Manutenção no SENAI. Eu tenho boas referências dos meu professores e muita motivação em ajudar o meu cliente. Tenho preferência em atender na cidade de Osasco.</p>
-                  </div>
+              <button id="botao">
+                <a href="#">Visitar Perfil</a>
+              </button>
             </div>
-            <button id="botao">
-              <a href="#">Visitar Perfil</a>
-            </button>
           </div>
         </div>
-      </div>
-
-      <div class="card mb-3" id="cartao3">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src={Profissional3} class="img-fluid " alt="Imagem Profissional do Autônomo" title="Pedro Lacerda Junior" id="imagem3"/>
-            <div id="avaliacao1">
-              <ul class="nav col-12" id="estrelas1">
-                <li id="nota">5.0</li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-                <li><i class="fa-solid fa-star"></i></li>
-              </ul>
-              <p class="card-text" id="profissao9"><small class="text-muted">Avaliação Profissional </small></p>
-           </div>
-          </div>
-          <div class="col-md-8">
-            <div class="card-body" id="texto1">
-              <h5 class="card-title" id="nome2">Pedro Lacerda Junior</h5>
-              <p class="card-text" id="profissao9"><small class="text-muted">Empreiteiro</small></p>
-              <div class="icones">
-                <ul class="nav col-12" id="informacoes3">
+        <div class="card mb-3" id="cartao3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src={Profissional3} class="img-fluid " alt="Imagem Profissional do Autônomo" title="Pedro Lacerda Junior" id="imagem3" />
+              <div id="avaliacao1">
+                <ul class="nav col-12" id="estrelas1">
+                  <li id="nota">5.0</li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                </ul>
+                <p class="card-text" id="profissao9"><small class="text-muted">Avaliação Profissional </small></p>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body" id="texto1">
+                <h5 class="card-title" id="nome2">Pedro Lacerda Junior</h5>
+                <p class="card-text" id="profissao9"><small class="text-muted">Empreiteiro</small></p>
+                <div class="icones">
+                  <ul class="nav col-12" id="informacoes3">
                     <li> <i class="fa-solid fa-location-dot"></i>   Osasco</li>
                     <li> <i class="fa-solid fa-hammer"></i>   25 Serviços</li>
                     <li> <i class="fa-solid fa-circle-check"></i>   Documentos Verificados</li>
-                </ul>
+                  </ul>
+                </div>
+                <div id="texto1">
+                  <p class="card-text">Tenho bastante disposição e habilidade. Atualmente possuo mais de 10 anos de experiência e boas referências, o que comprova a qualidade do meu trabalho. Possuo curso técnico, garantindo um serviço de qualidade por um ótimo preço! Venha fazer um orçamento! </p>
+                </div>
               </div>
-                  <div id="texto1">
-                    <p class="card-text">Tenho bastante disposição e habilidade. Atualmente possuo mais de 10 anos de experiência e boas referências, o que comprova a qualidade do meu trabalho. Possuo curso técnico, garantindo um serviço de qualidade por um ótimo preço! Venha fazer um orçamento! </p>
-                  </div>
+              <button id="botao">
+                <a href="#">Visitar Perfil</a>
+              </button>
             </div>
-            <button id="botao">
-              <a href="#">Visitar Perfil</a>
-            </button>
           </div>
         </div>
-      </div>
-
-      <div class="card" id="cartao5">
-        <div class="card-body">
-          <h5 class="card-title" id="titulocartao">Avaliação</h5>
-          <input type="range" class="form-range" id="customRange1" min="0" max="5"/>
-          <label for="customRange1" class="form-label">012345</label>
+        <div class="card" id="cartao5">
+          <div class="card-body">
+            <h5 class="card-title" id="titulocartao">Avaliação</h5>
+            <input type="range" class="form-range" id="customRange1" min="0" max="5" />
+            <label for="customRange1" class="form-label">012345</label>
+          </div>
         </div>
-      </div>
+        <div class="card" id="cartao5">
+          <div class="card-body">
+            <h5 class="card-title" id="titulocartao">Distância</h5>
+            <input type="range" class="form-range" id="customRange1" min="0" max="5" />
+            <label for="customRange1" class="form-label">012345</label>
+          </div>
+        </div>
+      </main>
 
-    <div class="card" id="cartao5">
-      <div class="card-body">
-        <h5 class="card-title" id="titulocartao">Distância</h5>
-        <input type="range" class="form-range" id="customRange1" min="0" max="5"/>
-        <label for="customRange1" class="form-label">012345</label>
-      </div>
+      <Footer />
+
     </div>
 
-    </main>
-        
-    );
-     }
-
-export default Reformas;
+  );
+}
