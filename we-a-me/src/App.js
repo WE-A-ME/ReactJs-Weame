29c0f604db598e4416b75ead21e3aa8a82bc6cf0
Danielly Alves Silva <101145664+daniellysilva@users.noreply.github.com>
2022-06-22 12:40:05 -0300

    Update App.js

--- we-a-me/src/App.js
+++ we-a-me/src/App.js
@@ -1,32 +1,25 @@
 import "./App.css";
-import { NavBarIndex } from "./components/Pages Componentes/NavBar Index/NavBarIndex";
-import { Footer } from "./components/Pages Componentes/Footer/footer";
-import { NavbarCategorias } from "./components/Pages Componentes/Navbar Categorias/NavbarCategorias";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Index } from "./components/Pages Thiago/1-Index";
 import { Categorias } from "./components/Page Danielly/Categorias/Categorias";
-import { Suporte } from "./components/Pages Thiago/Suporte/Suporte";
-import SobreNos from "./components/Page Danielly/SobreNos/SobreNos";
-import { Index } from "./components/Pages Thiago/1-Index/index";
-import { CadastroCliente } from "./components/Pages Thiago/CadastroCliente/cadastro";
-import { Reformas } from "./components/Page Danielly/LoginCategorias/Reformas/LoginCategoriasReformas";
-import { PaginaCliente } from "./components/Page Danielly/Pagina Inicial Cliente/PaginaCliente";
-
-
 
 function App() {
   return (
-    <div>
-      {/* <NavBarIndex/> OK  */}
-       <Index/>  
-      {/* <CadastroCliente/> OK */}
-      {/* <Categorias/>   */}
-      {/* <Suporte/> OK */}
-      {/* <SobreNos/> Detalhar  */}
-      {/* <Reformas/>   */}
-      {/* <Footer/> OK */}
-      {/* <PaginaCliente/>*/}
-     
+    <div >
+
+      <Router>
+        <Routes>
+        <Route path="/" element= {<Index/>} />
+        <Route path="/categorias" element={<Categorias/>} />
+        </Routes>
+      </Router>
+
     </div>
-  );
-}
+  )
+};
 
 export default App;
+
+
+
