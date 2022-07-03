5319f8e8d11c19898aff9dd054b310cc8bf9e37d
Tijigui <thiago.santos.guimaraes@outlook.com>
2022-07-03 03:40:11 -0300

    Alteração

--- we-a-me/src/components/Pages Thiago/6-PaginaEdicaoProfissional/PaginaEdicaoProfissional.jsx
+++ we-a-me/src/components/Pages Thiago/6-PaginaEdicaoProfissional/PaginaEdicaoProfissional.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import { Footer } from "../../Pages Componentes/Footer/footer";
 import { NavBarIndex } from "../../Pages Componentes/NavBar Index/NavBarIndex";
+import  "./PaginaEdicaoProfissional.css";
+import img_edicao from "../img/img_edição_profissional.svg";
 
 
 export function EdicaoProfissional() {
     return (
         <div>
             <NavBarIndex />
-            oi
+
+            <main>
+                <div className="container-">
+                    <div className="conteudo-img-profissional">
+                        <div className="img--profissional">
+                            <img src={img_edicao} alt="" />
+                        </div>
+                            <div className="info--profissional">
+                                <h2>Jorge dos Santos</h2>
+                                <p>Ajulegista</p>
+                            </div>
+                            <div className="icons">
+                            <i class="fa-solid fa-location-pin"></i>
+                            Salve
+
+                            <i class="fa-regular fa-hammer"></i>
+                            </div>
+                    </div>
+
+
+                  
+                </div>
+            </main>
+
             <Footer />
         </div>
     );
