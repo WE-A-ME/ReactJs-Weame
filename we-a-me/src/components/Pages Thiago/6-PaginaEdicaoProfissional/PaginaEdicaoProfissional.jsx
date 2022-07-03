a80a0c727d6e7df8b265fb8fcbee9c7be11b0195
Tijigui <thiago.santos.guimaraes@outlook.com>
2022-07-03 13:54:24 -0300

    Alteração

--- we-a-me/src/components/Pages Thiago/6-PaginaEdicaoProfissional/PaginaEdicaoProfissional.jsx
+++ we-a-me/src/components/Pages Thiago/6-PaginaEdicaoProfissional/PaginaEdicaoProfissional.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Footer } from "../../Pages Componentes/Footer/footer";
 import { NavBarIndex } from "../../Pages Componentes/NavBar Index/NavBarIndex";
-import  "./PaginaEdicaoProfissional.css";
+import "./PaginaEdicaoProfissional.css";
 import img_edicao from "../img/img_edição_profissional.svg";
 
 
@@ -16,20 +16,45 @@ export function EdicaoProfissional() {
                         <div className="img--profissional">
                             <img src={img_edicao} alt="" />
                         </div>
-                            <div className="info--profissional">
-                                <h2>Jorge dos Santos</h2>
-                                <p>Ajulegista</p>
-                            </div>
-                            <div className="icons">
-                            <i class="fa-solid fa-location-pin"></i>
-                            Salve
 
-                            <i class="fa-regular fa-hammer"></i>
-                            </div>
+                        <div className="info--profissional">
+                            <h2>Jorge dos Santos</h2>
+                            <h3>Ajulegista</h3>
+                        </div>
+
+                        <div className="info">
+                            <ul class="nav col-12" id="informacoes4">
+                                <li> <i class="fa-solid fa-location-dot"></i>  Osasco</li>
+                                <li> <i class="fa-solid fa-hammer"></i>  17 Serviços</li>
+                            </ul>
+                        </div>
                     </div>
 
+                    <section>
+                        <div className="boxinfos">
+                            <h2>Jorge dos Santos</h2>
+                            <div className="idade-grade">
+                                <h3>Idade</h3>
+                                {/* <img src= alt="" /> */}
+                                <h3>Formação</h3>
+                            </div>
+                            <form action="">
+                                <input type="text" placeholder="Adicione sua biografia" />
+                            </form>
+                        </div>
+                    </section>
 
-                  
+                    <section>
+
+                        <h2>Servicos Já Realizados</h2>
+
+                        <div className="btn-adicionar-servicos">
+                            <button> <i class="fa-solid fa-circle-plus"></i> Adicionar Serviços Realizados</button>
+                        </div>
+                    </section>
+
+
+
                 </div>
             </main>
 
