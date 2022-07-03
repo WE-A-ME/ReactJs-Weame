fc54312050362999374d2ccb3b6b1171d2ba619b
Tijigui <thiago.santos.guimaraes@outlook.com>
2022-07-03 16:39:47 -0300

    Alteração Cadastro Cliente

--- we-a-me/src/components/Pages Thiago/2-CadastroCliente/cadastro.jsx
+++ we-a-me/src/components/Pages Thiago/2-CadastroCliente/cadastro.jsx
@@ -9,26 +9,33 @@ import { Link } from "react-router-dom";
 export function CadastroCliente() {
   return (
     <div className="cadastropagina container-">
-      <Link to='/'><img className="header--logo" src={Logo} alt="" /></Link>
-      <div class="conteudo">
-        <div class="main--img">
-          <img src={img_cadastro} alt="" />
+      <header>
+        <Link to='/'><img className="header--logo" src={Logo} alt="" /></Link>
+      </header>
+
+
+      <div className="conteudo">
+        <div >
+          <img className="main--img" src={img_cadastro} alt="" />
         </div>
 
-        <div class="main--cadastro">
+        <div className="main--cadastro">
+
+
 
           <div className="main--title col-12">
             <h2 id="h1">Cadastra-se. <br /> É rápido e fácil </h2>
           </div>
 
-          <div class="main--inputs">
-            <input class="button-nome" type="text" placeholder="Nome Completo" />
-            <input class="button-email" type="text" placeholder="E-mail ou CPF/CNPJ" />
-            <input class="button-senha" type="password" placeholder="Senha" />
-            <Link to="/cadastrocliente-localizacao"><button class="btn-entrar">Cadastrar</button> </Link>
+          <div className="main--inputs">
+            <input className="button-nome" type="text" placeholder="Nome Completo" />
+            <input className="button-cpf" type="number" placeholder="CPF"  pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})"/>
+            <input className="button-email" type="text" placeholder="E-mail ou CPF/CNPJ" />
+            <input className="button-senha" type="password" placeholder="Senha" />
+            <Link to="/cadastrocliente-localizacao"><button className="btn-entrar">Cadastrar</button> </Link>
             <a href="/logincliente">Já tenho uma conta !</a>
-            <button class="btn--login--google"><img src={logo_google} alt="" /> Cadastrar com o Google</button>
-            <button class="btn--login--linkedin"><img src={logo_linkedin} alt="" /> Cadastrar com o Linkedin</button>
+            <button className="btn--login--google"><img className="img-btns" src={logo_google} alt="" /> Cadastrar com o Google</button>
+            <button className="btn--login--linkedin"><img className="img-btns" src={logo_linkedin} alt="" /> Cadastrar com o Linkedin</button>
           </div>
         </div>
       </div>
